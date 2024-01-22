ace3c3f246f99e24eeab46238f7e1c3f26f5cf73
GabrielCoelhoS <138086747+GabrielCoelhoS@users.noreply.github.com>
2024-01-22 08:22:23 -0300

    0.2.3.0

--- CalcJavScript.js
+++ CalcJavScript.js
@@ -1,26 +1,19 @@
 var bntValorInicial = document.querySelector('#button1');
 var bntRodadas = document.querySelector('#button2');
 var bntEspecial = document.querySelector('#button3');
-var bntEspecial2 = document.querySelector('#button4');
-var telaInicial = document.querySelector('#telaInicial');
-var ValorInicial = document.querySelector('#ValorInicial');
+var TelaInicial = document.querySelector('#TelaInicial');
+var ValorInicial = document.querySelector('.ValorInicial');
 var Rodadas = document.querySelector('#Rodadas');
 var Especial = document.querySelector('#Especial');
-var Especial2 = document.querySelector('#Especial2');
-var container = document.querySelector('.container');
 bntValorInicial.addEventListener('click', function() {
-    telaInicial.style.display = 'none';
+    TelaInicial.style.display = 'none';
     ValorInicial.style.display = 'block';
 });
 bntRodadas.addEventListener('click', function(){
-    telaInicial.style.display = 'none';
+    TelaInicial.style.display = 'none';
     Rodadas.style.display = 'block';
 });
 bntEspecial.addEventListener('click', function(){
-    telaInicial.style.display = 'none';
+    TelaInicial.style.display = 'none';
     Especial.style.display = 'block';
-});
-bntEspecial2.addEventListener('click', function(){
-    telaInicial.style.display = 'none';
-    Especial2.style.display = 'block';
 });
