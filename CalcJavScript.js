35057d5ddf59fe5d79596572620f1992cbe74707
GabrielCoelhoS <138086747+GabrielCoelhoS@users.noreply.github.com>
2024-01-22 08:12:39 -0300

    0.2.3.0

--- CalcJavScript.js
+++ CalcJavScript.js
@@ -9,23 +9,19 @@ var Especial = document.querySelector('#Especial');
 var Especial2 = document.querySelector('#Especial2')
 var btn = document.querySelector('#show-or-hide');
 var container = document.querySelector('.container');
-bntValorInicial.addEventListener('click', (event) => {
-    event.preventDefault();
+bntValorInicial.addEventListener('click', function() {
     telaInicial.style.display = 'none';
     ValorInicial.style.display = 'block';
 });
-bntRodadas.addEventListener('click', (event) =>{
-    event.preventDefault();
+bntRodadas.addEventListener('click', function(){
     telaInicial.style.display = 'none';
     Rodadas.style.display = 'block';
 });
-bntEspecial.addEventListener('click', (event) =>{
-    event.preventDefault();
+bntEspecial.addEventListener('click', function(){
     telaInicial.style.display = 'none';
     Especial.style.display = 'block';
 });
-bntEspecial2.addEventListener('click', (event) =>{
-    event.preventDefault();
+bntEspecial2.addEventListener('click', fubction(){
     telaInicial.style.display = 'none';
     Especial2.style.display = 'block';
 });
